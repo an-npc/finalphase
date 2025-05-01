3a2a34a1a8b2347bf6b81db97e2b1bbb138145d7
Melanie (Jnea) Steiner <118273237+an-npc@users.noreply.github.com>
2025-05-01 13:33:12 -0500

    final code submission

---
+++ UserReg/src/main/java/Servlet1.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=UTF-8");
+		final PrintWriter out = response.getWriter();
+		out.println("Hallo");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ UserReg/src/main/java/Servlet2.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+/**
+ * Servlet implementation class Servlet2
+ */
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		final PrintWriter out = response.getWriter();
+		
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String location = request.getParameter("location");
+		String gender = request.getParameter("gender");
+		String experience = request.getParameter("experience");
+		
+		String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n";
+		out.println(docType + 
+				"<html>\n" +
+				"<head><title>User Registration</title></head>\n"+
+				"<body>\n" +
+				"<h1 align=\"center\">Welcome " + name + "</h1>" +
+				"<ul>\n" +
+				"<li><b>Your name</b>: " + name + "\n" +
+				"<li><b>Your email</b>: " + email + "\n" +
+				"<li><b>Your location</b>: " + location + "\n" +
+				"<li><b>Your gender</b>: " + gender + "\n" +
+				"<li><b>Your experience</b>: " + experience + "\n" +
+				"</ul>\n" +
+				"</body></html>"
+				);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
